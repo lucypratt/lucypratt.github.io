6207049caba39d586eda1d40faea1e5511a3c48a
Lucy Steckler <prattluc000@gmail.com>
2022-10-06 14:57:06 -0600

    Finished site

--- band/js/scripts.js
+++ band/js/scripts.js
@@ -1,3 +1,7 @@
+var lazyLoadInstance = new LazyLoad({
+    elements_selector: ".lazy",
+    threshold: 0
+   });
 function toggleMenu() {
     document.getElementById("navList").classList.toggle("open");
     document.getElementById("navButton").classList.toggle("open");
@@ -6,3 +10,6 @@ function toggleMenu() {
     const x = document.getElementById("navButton");
     
     x.onclick = toggleMenu;
+
+   
+     
